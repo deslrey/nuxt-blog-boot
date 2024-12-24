499155a8cf14ddcf7a1cd21e4fc9500c152ea790
deslrey <2135232170@qq.com>
2024-12-24 10:51:30 +0800

    feat: 上传统一返回结果类

---
+++ src/main/java/org/deslre/result/ResultCodeEnum.java
@@ -0,0 +1,42 @@
+package org.deslre.result;
+
+import lombok.Getter;
+
+/**
+ * 统一返回结果状态信息类
+ */
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200, "请求成功"),
+    FAIL(201, "失败"),
+    EMPTY_VALUE(202, "传入的参数存在空值"),
+    SERVICE_ERROR(2012, "服务异常"),
+    DATA_ERROR(204, "数据异常"),
+    ILLEGAL_REQUEST(205, "非法请求"),
+    REPEAT_SUBMIT(206, "重复提交"),
+    ARGUMENT_VALID_ERROR(210, "参数校验异常"),
+
+    LOGIN_AUTH(208, "未登陆"),
+    PERMISSION(209, "没有权限"),
+    ACCOUNT_ERROR(214, "账号不正确"),
+    PASSWORD_ERROR(215, "密码不正确"),
+    LOGIN_MOBILE_ERROR(216, "账号不正确"),
+    ACCOUNT_STOP(217, "账号已停用"),
+
+    CODE_404(404, "请求地址不存在"),
+    CODE_600(600, "请求参数错误"),
+    CODE_601(601, "信息已经存在"),
+    CODE_500(500, "服务器返回错误，请联系管理员"),
+    CODE_901(901, "登录超时，请重新登录"),
+    CODE_904(904, "空间不足，请扩容");
+
+
+    private final Integer code;
+    private final String message;
+
+    ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
