b336f5b6ea06126f68c7c9c6f0a348316bbb509b
deslrey <2135232170@qq.com>
2024-12-24 10:39:41 +0800

    feat: 第一次提交后端项目

---
+++ src/main/java/org/deslre/DeslreBlogApplication.java
@@ -0,0 +1,13 @@
+package org.deslre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeslreBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeslreBlogApplication.class, args);
+    }
+
+}
