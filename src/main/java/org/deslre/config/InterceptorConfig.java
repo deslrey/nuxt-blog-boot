3ca43e3b2b73f7a7c24bf35af0ca707f8ffe40cf
deslrey <2135232170@qq.com>
2024-12-24 14:57:55 +0800

    feat: 将系统的文件进行映射,可以通过url进行访问该图片

---
+++ src/main/java/org/deslre/config/InterceptorConfig.java
@@ -0,0 +1,16 @@
+package org.deslre.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    private final String imagePath = "E:\\images";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + imagePath + "\\"); // 注意末尾加上 "\\"
+    }
+}
