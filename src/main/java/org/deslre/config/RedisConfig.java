5a90c0303d4b5526f54b299af0eb45a01b9ee02d
deslrey <2135232170@qq.com>
2024-12-24 23:01:00 +0800

    feat: 添加redis配置类

---
+++ src/main/java/org/deslre/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package org.deslre.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * ClassName: RedisConfig
+ * Description: TODO
+ * Author: Deslrey
+ * Date: 2024-07-28 21:52
+ * Version: 1.0
+ */
+@Configuration
+public class RedisConfig<V> {
+
+    @Bean
+    public RedisTemplate<String, V> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, V> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // 设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
